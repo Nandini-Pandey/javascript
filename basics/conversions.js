47ede90f1c5d949d0b916e25619f4dd2431c8f14
Nandini Pandey <120239212+Nandini-Pandey@users.noreply.github.com>
2024-01-14 17:03:59 +0000

    conversionsdone

---
+++ basics/conversions.js
@@ -0,0 +1,15 @@
+let num="33"
+let value=Number(num)
+console.log(typeof value)
+console.log(typeof num)
+console.log(value)
+let val2="33ab"
+let val3=Number(val2)
+console.log(typeof val2)
+console.log(typeof val3);
+console.log(val3)
+
+// variableval      after conversion
+// "33"                   33
+//"33ab"                NaN
+// true                 1
