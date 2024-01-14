327a1930665c70a9c6efffed07c18258e14d70a4
Nandini Pandey <120239212+Nandini-Pandey@users.noreply.github.com>
2024-01-14 11:53:44 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code of newer version
+//alert(3+3)  we are using nodejs and not browser
+
+// important documentations : mdn , tc.39ec(ECMA script)
+
+let name="nnppmm"
+let age=18
+let isLoggedin=true
+
+
+// all datatypes:
+
+// number limit 2^53
+// string ""
+// bigint
+//null(standalone value an object)
+// undefine(variable declared but no value assigned)
+// symbol ( for uniqueness)
+
+// objects 
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
